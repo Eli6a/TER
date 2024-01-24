183495154bf491deada656f92119239cc5a980ee
Eli6a <sisa.elis@gmail.com>
2024-01-24 14:33:30 +0100

    spacy et nltk segmentation

--- source/pipeline.py
+++ source/pipeline.py
@@ -1,12 +1,17 @@
 import segmentation, eval
-from tokenize import tokenizer
+#from tokenize import tokenizer
 
 
-def pipeline(path, model='tok'):
-    text = file(path)
-    raw_toks = tokenizer(text)
-    seg = segmentation(text)
-    eval(seg, raw_toks)
+def pipeline(path='C:\\Users\\elisa\\Downloads\\TER\\documents\\train\\ROSENBLATT v. BAER_MCL.txt', model='spacy'):
+    text = ''
+    
+    with open(path, 'r') as fichier:
+        text = fichier.read()
+    
+   # raw_toks = tokenizer(text)
+    seg = segmentation.segmentation('ROSENBLATT v. BAER_MCL', text, model='spacy')
+    seg2 = segmentation.segmentation('ROSENBLATT v. BAER_MCL', text, model='nltk')
+   # eval(seg, raw_toks)
 
 
 if __name__ == "__main__":
--- source/segmentation.py
+++ source/segmentation.py
@@ -1,4 +1,47 @@
-from tokenize import tokenizer
+# from tokenize import tokenizer
+import os
 
-def segmentation(text, model='main'):
-    tokenizer(text, model)
+def segmentation(title, text, model='main'):
+    # tokenizer(text, model)
+    
+    if model == 'spacy':
+        return segmentation_spacy(title, text)
+    
+    if model == 'nltk':
+        return segmentation_nltk(title, text)
+    
+def segmentation_spacy(title, text):
+    import spacy
+
+    script_directory = os.path.dirname(__file__)
+    input_file_path = os.path.join(script_directory, '../documents/train/' + title + '.txt')
+    output_file_path = os.path.join(script_directory, '../outputs/spacy/' + title + '.txt')
+
+    nlp = spacy.load("en_core_web_sm")
+    doc = None
+    
+    with open(input_file_path, 'r') as f:
+        doc = nlp(f.read())
+    
+    with open(output_file_path, 'w') as f:
+        for sent in doc.sents:
+            f.write(sent.text + '\n')
+            
+    return '../outputs/spacy_'+ title +'.txt'
+
+def segmentation_nltk(title, text):
+    from nltk.tokenize import sent_tokenize
+
+    script_directory = os.path.dirname(__file__)
+    output_file_path = os.path.join(script_directory, '../outputs/nltk/' + title + '.txt')
+        
+    try:
+        with open(output_file_path, 'w') as f:
+            for sent in sent_tokenize(text):
+                f.write(sent + '\n')
+        return output_file_path
+    
+    except Exception as e:
+        print(f"Erreur lors de l'écriture du fichier : {e}")
+        return None
+ 
